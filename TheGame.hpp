3c6979e377eda21bb5a1ced4088794d2ee8d8138
Firat Atalay <firatatalay@gmail.com>
2017-03-29 03:36:45 +0300

    Create TheGame.hpp

---
+++ TheGame.hpp
@@ -0,0 +1,20 @@
+//
+//  TheGame.hpp
+//  TheGame
+//
+//  Created by Firat Atalay on 27/03/2017.
+//  Copyright © 2017 Firat Atalay. All rights reserved.
+//
+
+#ifndef TheGame_hpp
+#define TheGame_hpp
+
+#include <stdio.h>
+#include <iostream>
+#include <random>
+using namespace std;
+
+#include "Card.hpp"
+#include "Deck.hpp"
+
+#endif /* TheGame_hpp */
